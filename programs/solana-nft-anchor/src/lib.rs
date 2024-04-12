7d48b577942e651d947dd48600a94adfcae2d764
namnampe <166782667+namnampe@users.noreply.github.com>
2024-04-12 11:08:07 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("58DRiHRxt5Ru8TPezTVr8iNsCGJDHpFHUft98ZxCdGxk");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
